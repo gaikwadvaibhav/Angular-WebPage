4d6f01d964c6bac5f9e722226a723f92041af820
vaibhav0322 <gaikwadvaibhav03@gmail.com>
2017-07-25 23:23:33 +0530

    directive component

--- src/app/directive/directive.component.ts
+++ src/app/directive/directive.component.ts
@@ -1,15 +1,25 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnChanges } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
 
 @Component({
   selector: 'app-directive',
   templateUrl: './directive.component.html',
   styleUrls: ['./directive.component.css']
 })
-export class DirectiveComponent implements OnInit {
+export class DirectiveComponent implements OnInit, OnChanges  {
 
+textColor : string;
+fullColor : string;
   constructor() { }
 
   ngOnInit() {
+      this.fullColor = '1px solid '+this.textColor;
+
   }
+ngOnChanges(){
+  this.fullColor = '1px solid '+this.textColor;
+  console.log(`Full Color ${this.fullColor}`);
+}
 
 }
